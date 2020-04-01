eeb1b858d433741f01ec6132d6e2d76e9a790765
Paul Rutledge <rutledgepaulv@gmail.com>
2020-03-31 23:50:45 -0500

    cut release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.rutledgepaulv/defcash "0.1.0"
+(defproject org.clojars.rutledgepaulv/defcash "0.1.1-SNAPSHOT"
 
   :description
   "A macro that combines clojure.core.memoize and defn"
